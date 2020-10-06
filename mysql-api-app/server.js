540454c1077f461c5b63e71965ae0117e06abad4
Luke Williams <law67@students.uwf.edu>
2020-10-06 10:50:42 -0500

    server.js altered to reflect new PORT and db sync

--- mysql-api-app/server.js
+++ mysql-api-app/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:3000"
+  origin: "http://localhost:4000"
 };
 
 app.use(cors(corsOptions));
@@ -16,15 +16,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // link to db
 const db = require("./app/models");
-db.sequelize.sync();
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and re-sync db.");
+});
 
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "MySql API" });
 });
 
+// user routes
+require("./app/routes/user.routes")(app);
+
 // set port, listen for requests
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });

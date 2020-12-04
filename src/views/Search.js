f6182d98ad76dcce66ea6c5fb58ca49b4c8bf9e7
Taylor Hall <th65@students.uwf.edu>
2020-12-04 02:48:34 -0600

    Fixing display of personal chat rooms

--- src/views/Search.js
+++ src/views/Search.js
@@ -110,31 +110,29 @@ class Search extends Component {
 
   render() {
     let results = [];
-    let resultsTitle;
+    let resultsTitle = "";
     for (let i = 0; i < this.state.results.length; i++) {
       let friendStatus;
       if (this.state.results[i].friendIcon === "Friends") {
-        friendStatus = (
-          <div>
-            <button className="search__viewProfileButton">View Profile</button>
-          </div>
-        );
+        friendStatus = (  <i
+          className="search__addIcon material-icons"
+          onClick={this.createChat}
+        >
+          chat
+        </i>);
       } else {
-        friendStatus = (
-          <i
-            className="search__addIcon material-icons"
-            onClick={() => this.addFriend(this.state.results[i].friend._id)}
-          >
-            add
-          </i>
-        );
-      }
+        friendStatus = (  <i
+          className="search__addIcon material-icons"
+          onClick={() => this.addFriend(this.state.results[i].friend._id)}
+        >
+          add
+        </i>);      }
       results.push(
         <div
           className="search__resultsCard"
           key={i}
           id={"userCard" + i}
-          username={this.state.results[i].username}
+          username={this.state.results[i].friend.username}
         >
           <i className="search__resultsIcon material-icons">person</i>
           <p className="search__resultsName">
@@ -148,12 +146,6 @@ class Search extends Component {
             <br></br>
           </p>
           {friendStatus}
-          <i
-              className="search__chatIcon material-icons"
-              onClick={this.createChat}
-            >
-              chat
-            </i>
         </div>
       );
     }
